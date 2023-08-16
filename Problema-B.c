fbdd8789b7cd88935a252bd4a8133eb737ae79e3
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2023-08-16 18:06:38 -0300

    Create Problema-B.c

---
+++ Problema-B.c
@@ -0,0 +1,22 @@
+//Ptoblema B - Escolha científica
+
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    while (1) {
+        scanf("%d", &N);
+
+        if (N == -1) {
+            break;
+        }
+        // O mantra de escolha
+        const char *mantra = "Mi-nha-mãe-man-dou-eu-es-co-lher-es-te-da-qui-mas-co-mo-sou-de-so-be-di-en-te-vou-es-co-lher-es-te-da-qui";
+        int mantraLength = 65; // Comprimento do mantra
+
+        int chosenVehicle = (mantra[N % mantraLength * 2] - 'a' + 1) % N + 1;
+        printf("%d\n", chosenVehicle);
+    }
+    return 0;
+}
