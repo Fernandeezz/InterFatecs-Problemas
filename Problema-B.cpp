4e44e7c80a5dd16610254e6394322eb9c39e6449
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2023-08-16 17:45:08 -0300

    Create Problema-B.cpp

---
+++ Problema-B.cpp
@@ -0,0 +1,31 @@
+// Problema B - Escolha científica
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int chooseVehicle(int N) {
+    string mantra = "Mi-nha-mãe-man-dou-eu-es-co-lher-es-te-da-qui-mas-co-mo-sou-de-so-be-di-en-te-vou-es-co-lher-es-te-da-qui";
+    int idx = 0;
+    for (int i = 0; i < mantra.length(); i++) {
+        if (mantra[i] == '-') {
+            idx++;
+            if (idx == N) {
+                idx = 0;
+            }
+        }
+    }
+    return idx + 1;
+}
+int main() {
+    int N;
+    while (true) {
+        cin >> N;
+        if (N == -1) {
+            break;
+        }
+        int chosenVehicle = chooseVehicle(N);
+        cout << chosenVehicle << endl;
+    }
+    return 0;
+}
