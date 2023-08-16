153ad79275554bb639dd89f4c4ac71e9641f8836
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2023-08-16 18:14:32 -0300

    Create Problema-E.c

---
+++ Problema-E.c
@@ -0,0 +1,37 @@
+//Problema E - Prefeito Tecnológico
+
+#include <stdio.h>
+
+typedef struct {
+    int custo;
+    int votos;
+} Melhoria;
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    while (N--) {
+        int T, L;
+        scanf("%d %d", &T, &L);
+        Melhoria melhorias[L];
+        for (int i = 0; i < L; i++) {
+            scanf("%d %d", &melhorias[i].custo, &melhorias[i].votos);
+        }
+        int maxVotos = 0;
+        int custoTotal = 0;
+        for (int i = 0; i < L; i++) {
+            if (melhorias[i].custo <= T) {
+                if (melhorias[i].votos > maxVotos) {
+                    maxVotos = melhorias[i].votos;
+                    custoTotal = melhorias[i].custo;
+                }
+            }
+        }
+        if (maxVotos == 0) {
+            printf("NO FUNDS\n");
+        } else {
+            printf("%d %d\n", maxVotos, T - custoTotal);
+        }
+    }
+    return 0;
+}
